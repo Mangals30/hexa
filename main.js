d96435508f306b0bef6fb55841418dd259e45e0a
Mangals30 <mangalam301185@gmail.com>
2020-01-12 20:18:09 +0200

    created second branch

--- main.js
+++ main.js
@@ -1,96 +1,114 @@
-const randomHexaNumber = () => {
+/* Declaration of global variables*/
+const searchResultDiv = document.querySelector('#searchResult')
+const generate = document.querySelector('#generate')   
+const stop = document.querySelector('#stop') 
+let flag = ''
+let colorTimer = '' 
+
+/*Function to generate random hexacolor id*/ 
+const randomHexaId = () => {
     let hexaChar= '0123456789abcdef'
     let hexaLen = hexaChar.length
-    //let hexaIndex = Math.floor(Math.random()*hexaLen)
     let hexaId = '#'
     for (let i=0; i<6; i++) {
-        hexaId = hexaId + hexaChar[Math.floor(Math.random()*hexaLen)]
+        let hexaIdIndex = Math.floor(Math.random()*hexaLen)
+        hexaId = hexaId + hexaChar[hexaIdIndex]
 
     }
     
     return hexaId
 }
 
-const copyHexaId = (value) => {
+generate.addEventListener('click', function() {
+    generateColors()
+})
+
+stop.addEventListener('click', function() {
+    stopTimer()
+})
+
+const copyHexaId = (id) => {
     let input = document.createElement('input')
     document.body.appendChild(input)
-    input.value = value
+    input.value = id
     input.select()
     document.execCommand('copy')
     input.remove()
 
 }
 
-const searchResultDiv = document.querySelector('#searchResult')
-const number = document.querySelector('#number')
-const generate = document.querySelector('#generate')   
-const stop = document.querySelector('#stop')  
+const stopTimer = () => {
+    clearInterval(homePageTimer)
+    clearInterval(colorTimer)
+}
+
      const homePage = () => {
      searchResultDiv.textContent = ''    
-    for (i=0; i<5;i++) {
-        const divCol = document.createElement('div')
-        const copyButton = document.createElement('button')
-        copyButton.style.margin = '10px'
-        copyButton.textContent= 'Copy'
-        //copyButton.setAttribute('id','copyid-'+i)
-        divCol.setAttribute('class','divCol')
-        const hexaColorId = randomHexaNumber()
-        divCol.append(hexaColorId)
-        divCol.append(copyButton)
-        divCol.style.background = hexaColorId
-        searchResultDiv.append(divCol)
-        copyButton.addEventListener('click', function(event) {
-            let copiedText = divCol.textContent.replace('Copy','')
-            copyHexaId(copiedText)
-            
-        })
-        
+    for (let i=0; i<5;i++) {
+        divCreate()
     }
 }
-  
+
+const errorMessage = () => {
+    searchResultDiv.textContent = ''
+    const divColors = document.createElement('div')
+    let errorMessage = '*Enter a number greater than or equal to 5'
+    divColors.textContent = errorMessage
+    divColors.style.color = 'red'
+    searchResultDiv.append(divColors)
+}
+const divCreate = () => {
+    const divColors = document.createElement('div')
+    divColors.setAttribute('class','divColors')
+    const copyButton = document.createElement('button')
+    copyButton.setAttribute('class','copy')
+    copyButton.style.margin = '10px'
+    copyButton.textContent= 'Copy'
+    const hexaColorId = randomHexaId()
+    divColors.append(hexaColorId)
+    divColors.append(copyButton)
+    divColors.style.background = hexaColorId
+    searchResultDiv.append(divColors)
+    copyButton.addEventListener('click', function(event)
+       {
+         let copiedText = divColors.textContent.replace('Copy','')
+         copyHexaId(copiedText)
+            
+        })
+}
+
+const genColorTimer = () => {
+    colorTimer = setInterval (innerFunction,2000)
+}
+
+const innerFunction = () => {
+    const num = document.querySelector('#number')
+    if (num.value == flag) {
+        searchResultDiv.textContent = ''
+    for (let i=0; i<num.value;i++) {  
+        divCreate(num.value)  
+    }
+}
+}
+const generateColors = () => {
+    stopTimer()
+    const number = document.querySelector('#number')
+    flag = number.value
+    if (flag <5) {
+        searchResultDiv.textContent = ''
+        errorMessage()
+}
+else {
+   
+    innerFunction()
+    genColorTimer()
+    
+}
+}
+
+
+
+
   homePage()
 
-const timer = setInterval(homePage,2000)
- 
-stop.addEventListener('click', function() {
-    clearInterval(timer)
-})
-
-generate.addEventListener('click', function() {
-    clearTimeout(timer)
-    console.log('inside generator')
-    searchResultDiv.textContent = ''
-    const number = document.querySelector('#number')
-    console.log('Number is',number.value)
-     if ( number.value <5) {
-        divNum1 = document.createElement('div')
-        divNum1.textContent = '*Enter a number greater than or equal to 5'
-        divNum1.style.color = 'red'
-        searchResultDiv.append(divNum1)
-       //searchResultDiv.replaceWith(divNum1)
-       //searchResultDiv.replaceWith(divNum1)
-       const searchResultDiv1 = document.querySelector('#searchResult')
-       console.log('searchResultDiv in the middle',searchResultDiv1)
-    }
-    else {
-        const divNum2 = document.createElement('div')
-        divNum2.setAttribute('id','divNum2')
-        for (i=0; i<number.value;i++) {
-            const divNum3 = document.createElement('div')
-            divNum3.setAttribute('class','divNum3')
-            const hexaColorId = randomHexaNumber()
-            divNum3.append(hexaColorId)
-            console.log('divNum3,',divNum3)
-            divNum3.style.background = hexaColorId
-            divNum2.append(divNum3)
-            console.log('divNum2,',divNum2)
-        }
-        
-        searchResultDiv.append(divNum2)
-        console.log('Final divNum2', divNum2)
-        console.log(divNum2.textContent)
-        console.log('searchResultDiv before appending',searchResultDiv)
-        //searchResultDiv1.append(divNum2)
-        console.log('searchResultDiv after appending',searchResultDiv)
-        }
-})
+const homePageTimer = setInterval(homePage,2000)
